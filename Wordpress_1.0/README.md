901f37406b467082ec9c7a9c7e89b21c48067545
Mdcowse <pcpcpc1337@gmail.com>
2020-03-24 11:21:37 +0300

    Added Wordpress_1.0 scenario

---
+++ Wordpress_1.0/README.md
@@ -0,0 +1,36 @@
+![Image](../images/logo.png?raw=true)
+
+# About scenario
+This scenario allows you to quickly deploy (~5mins) your wordpress project to
+VM lab environment on Windows host.
+It can be useful when you already have wordpress projects and you need to test
+new features that will be deployed in production environment.
+
+No need to store VM files on your storage permanently.
+You can deploy lab environment on demand and remove when your tests are done.
+
+## Prerequisites
+Please read README.md in project root folder.
+Also you need to archive your root wordpress folder project and have sql dump.
+
+## Getting Started
+There are two simple steps.
+
+1) Copy wordpress project zip archive and sql dump to
+vagrant-sandboxie/provision/wordpress/wp-project folder and name files as
+archive.zip and db.sql.
+
+2) Open cmd or powershell,
+navigate to vagrant-sandboxie/provision/Wordpress_1.0 folder and run:
+
+```vagrant up```
+
+When deployment will be done you will be informed in console
+that environment is ready to use.
+
+## Removing lab environment
+
+Open cmd or powershell,
+navigate to vagrant-sandboxie/provision/Wordpress_1.0 folder and run:
+
+```vagrant destroy```
---
+++ provision/wordpress/wp-config.php
@@ -0,0 +1,39 @@
+<?php
+
+define( 'DB_NAME', 'wordpress' );
+
+/** MySQL database username */
+define( 'DB_USER', 'root' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
